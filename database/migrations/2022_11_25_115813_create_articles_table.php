21830d618b107d853be49a79978124d2766b9113
carly <n.carly91@gmail.com>
2022-11-28 20:44:23 +0900

    We editted the migration files by following the instruction.

---
+++ database/migrations/2022_11_25_115813_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->string('image');
+            $table->unsignedBigInteger('user_id')->nullable(); // owner_id->user_id
+            $table->unsignedBigInteger('establishment_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('establishment_id')->references('id')->on('establishments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
