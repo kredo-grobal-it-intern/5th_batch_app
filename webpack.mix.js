af9c31109b4e981610e6987435168076f597abaa
carly <n.carly91@gmail.com>
2022-12-16 22:19:26 +0900

    Revert "Related to Auth function."
    This reverts commit b722868536a8760746c17d8c2c8523013b887b3a.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,14 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    // .postCss('resources/sass/app.scss', 'public/css', [
+    //     //
+    // ])
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sass('resources/sass/q_and_a.scss', 'public/css');
