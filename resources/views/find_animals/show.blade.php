8a2fab58508e9daf9c16b478fed494286b89abd3
6060takuya <irta19320@g.nihon-u.ac.jp>
2022-12-30 21:13:23 +0900

    Find_Animal_Page View

---
+++ resources/views/find_animals/show.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('title', 'Donate page')
+
+@section('style')
+     <link rel="stylesheet" href="{{ mix('/css/show_find_animal.css') }}">
+@endsection
+
+@section('content')
+   <div class="p_form">
+    <div class="row mt-5">
+        <div class="col">
+            <img src="{{ asset('/storage/images/' . $publication->image) }}" alt="{{ $publication->image }}" class="w-75">
+            <div class="font_con">Registered Date:{{ $publication->created_at }}</div>
+            <div class="font_con">Updated at:{{ $publication->updated_at }}</div>
+        </div>
+
+        <div class="col">
+            <button  type="submit" class="back_button" ><a href="{{ route('find_animal.index') }}" class="link-dark">Back</a></button>
+             {{-- if the owner of the post is the Auth user, show Edit and Delete button --}}
+
+            <button  type="submit" class="edit_button" ><a href="{{ route('find_animal.find_animal.edit',$publication->id) }}" class="link-dark">Edit</a></button>
+
+            <form action="{{ route('find_animal.find_animal.destroy',$publication->id) }}" method="post" class="d-inline">
+                @csrf {{-- cross site reference forgeries --}}
+                @method('DELETE')
+
+                <button type="submit" class="delete_button">Delete</button>
+            </form>
+
+
+
+                <div class="row my-1">
+                    <div class="col font_con">pet_name</div>
+                    <div class="col font_con">age</div>
+                </div>
+                <div class="row my-1">
+                    {{-- name --}}
+                    <div class="col h5">{{ $publication->name }}
+                    </div>
+                {{-- age --}}
+                <div class="col h5">{{ $publication->date_of_brith }}
+                </div>
+                </div>
+
+                <div class="row my-1">
+                    <div class="col font_con">breed</div>
+                    <div class="col font_con">weight</div>
+                </div>
+                <div class="row my-1">
+                    {{-- breed --}}
+                    <div class="col h5">{{ $publication->breed }}</div>
+                {{-- weight --}}
+                    <div class="col h5">{{ $publication->weight }}
+                    </div>
+                </div>
+
+                <div class="row my-1">
+                    <div class="col font_con">Gender</div>
+                    <div class="col font_con">Kind</div>
+                </div>
+                <div class="row my-1">
+                    {{-- Gender --}}
+                    <div class="col h5">{{ $publication->gender }}</div>
+                    {{-- kind --}}
+                    <div class="col h5">{{ $publication->pet_type }}</div>
+                </div>
+
+                <div class="row my-1">
+                    <div class="col font_con">Spaying</div>
+                    <div class="col font_con">Vaccine</div>
+                </div>
+                <div class="row my-1">
+                    {{-- Spaying --}}
+                    <div class="col h5">{{ $publication->netured_status }}</div>
+                    {{-- Vaccine --}}
+                    <div class="col h5">{{ $publication->vaccination_status }}</div>
+                </div>
+
+                <div class="row my-1">
+                    {{-- Delivery Area --}}
+                    <div class="col font_con">Location prefecture</div>
+                    <div class="col font_con">URL</div>
+                </div>
+                <div class="row my-1">
+                    {{-- Delivery Area --}}
+                    <div class="col"><select type="checkbox" class="form-control" name="area">
+                        @foreach(config('pref') as $key => $score)
+                            <option value="{{ $score }}">{{ $score }}</option>
+                        @endforeach
+                    </select>
+                    </div>
+                    {{-- URL --}}
+                    <div class="col h5">{{ $publication->url }}</div>
+                </div>
+
+                <label for="Characteristics" class="my-1 font_con">Characteristics</label>
+            {{-- charateristic --}}
+            <div class="h5">{{ $publication->charateristic }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="pet_name">{{ $publication->name }}</div>
+    @endsection
