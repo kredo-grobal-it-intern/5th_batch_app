3b6e0282f55e4d343ce508a8ecfe5ac83c5098b0
6060takuya <irta19320@g.nihon-u.ac.jp>
2023-01-23 21:37:10 +0900

    change name Find Animal →　Find a Pet

--- resources/views/donates/animal_care.blade.php
+++ resources/views/donates/animal_care.blade.php
@@ -11,7 +11,7 @@
     <div class="p-3 m-2 text-white  mx-auto row"  style=" height: 340px;background:#F8F8F8;">
         <div class="col p-4">
             <div class="text-right"><a href="{{ route('find_animal.index') }}"><i class="fa-solid fa-dog link-warning w-25" style="font-size: 40px "></i></a></div>
-            <div class="text-right text-bold h3 text-dark pt-1">Find Animal</div>
+            <div class="text-right text-bold h3 text-dark pt-1">Find a Pet</div>
             <div class="text-right text-dark pt-1">You can find a dog you like and contact the owner.Most of the animals listed here are protected according to guidance from government agencies (police and protection).</div>
         </div>
         <div class="col p-4">
