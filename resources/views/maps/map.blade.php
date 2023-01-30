f58592cca0d8d193b37cc3c48315f50f6f867081
Takumi Kasuga <takumi.11b.ball@gmail.com>
2023-01-30 22:11:40 +0900

    changed to asset

--- resources/views/maps/map.blade.php
+++ resources/views/maps/map.blade.php
@@ -24,8 +24,8 @@
 
     @section('scripts')
         @parent
-        <script src="{{ mix('js/map.js') }}"></script>        
-        <script src="{{ mix('js/categories.js') }}"></script>      
+        <script src="{{ asset('js/map.js') }}"></script>        
+        <script src="{{ asset('js/categories.js') }}"></script>      
         
         <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.mapApiKey.key') }}&callback=initMap&libraries=places&v=weekly" defer></script>
     @stop
